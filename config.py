359464f1dec6dfb1bda3fdd1b6bac9fc5f62a25d
Aleksei Seliverstov <alexseliverstow@gmail.com>
2024-01-08 13:11:01 +0300

    Update config.py to use environment variables

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-NOTION_API_KEY=''
-BOT_TOKEN=''
-USER_ID=''
-DATABASE_ID=''
+import os
+NOTION_API_KEY = os.environ.get('NOTION_API_KEY')
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+USER_ID = os.environ.get('USER_ID')
+DATABASE_ID = os.environ.get('DATABASE_ID')
