c334edd0df4256e239771017314fe5db615047d5
Aleksei Seliverstov <alexseliverstow@gmail.com>
2024-01-10 13:17:03 +0300

    Refactor main.py: Improve code formatting and add webhook support

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
 import config
 from notion import create_notion_page
+import os
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     create_notion_page(update.message.text)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Created notion task: {update.message.text}")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Created notion task: {update.message.text}",
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application = ApplicationBuilder().token(config.BOT_TOKEN).build()
 
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler("start", start)
     echo_handler = MessageHandler(filters.TEXT, echo)
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
 
-    application.run_polling()
+    port = int(os.environ.get('PORT', 5000))
+    application.run_webhook(
+        listen="0.0.0.0", port=port, url_path=config.BOT_TOKEN
+    )
